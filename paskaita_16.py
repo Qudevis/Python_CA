65d2d758a72824ddd0a09b72dcf8a43f05a26321
Justas Kvederis <justas@computech.ie>
2024-06-07 15:33:19 +0300

    Make Changes

--- paskaita_16.py
+++ paskaita_16.py
@@ -1,5 +1,4 @@
 import sqlalchemy as sqla
-
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = sqla.create_engine("sqlite:///main_database.db")
@@ -7,13 +6,13 @@ engine = sqla.create_engine("sqlite:///main_database.db")
 Base = declarative_base() # pagrindinis sqlalchemy elementas, kuriame yra surisimo (klases su lentele) logika
 
 class Person(Base):
-    __tablename__ = "People"
+    __tablename__ = "People" # cia skirta lenteles sukurimui
     id = sqla.Column(sqla.Integer,primary_key=True)
     name = sqla.Column("Name",sqla.String)
     last_name = sqla.Column("Last_Name",sqla.String)
     age = sqla.Column("Age",sqla.Integer)
 
-    def __init__(self,name,last_name,age):
+    def __init__(self,name,last_name,age): # cia jau kalba eina apie pati objekta Person
         self.name = name
         self.last_name = last_name
         self.age = age
