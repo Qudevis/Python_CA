25721cfe7111302765b8cb655db85ca920ba6899
Justas Kvederis <justas@computech.ie>
2024-06-12 11:07:30 +0300

    alembic examples

---
+++ Paskaita_17.py
@@ -0,0 +1,21 @@
+import sqlalchemy as sqla
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = sqla.create_engine("sqlite:///main_database.db")
+
+Base = declarative_base() # pagrindinis sqlalchemy elementas, kuriame yra surisimo (klases su lentele) logika
+
+class Person(Base):
+    __tablename__ = "People" # cia skirta lenteles sukurimui
+    id = sqla.Column(sqla.Integer,primary_key=True)
+    name = sqla.Column("Name",sqla.String)
+    last_name = sqla.Column("Last_Name",sqla.String)
+    age = sqla.Column("Age",sqla.Integer)
+
+    def __init__(self,name,last_name,age): # cia jau kalba eina apie pati objekta Person
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+    def __repr__(self):
+        return f"{self.name}|{self.last_name}"
+
