d425c7aad5a595aca631062a84274dca8f9b1ce8
Justas Kvederis <justas@computech.ie>
2024-06-13 14:52:42 +0300

    Tkinter

---
+++ Paskaita_19_2.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+main_window = Tk()
+main_window.geometry("500x500")
+
+sb = Scrollbar(main_window)
+
+box = Listbox(main_window, yscrollcommand=sb.set, selectmode=SINGLE) # yscrollcommand prisirisa prie scrolbaor pasukimo (sukasi, kai judiname scrollbara)
+sb.config(command=box.yview) # scrollbaras prisirisa prie box ir juda kai judiname box
+
+# box.insert(END,"Justas")
+
+numbers = range(1,50)
+
+box.insert(END,*numbers)
+
+sb.pack(side=RIGHT, fill=Y)
+box.pack(side=LEFT)
+
+box.bind("<Button-3>",lambda x: print(numbers[box.curselection()[0]]))
+
+main_window.mainloop()
+
